492a5731ec73fe63218425297bc3da6deec3c0b1
TAYYAB-SIDDIQUI <tayyabsiddiqui9892@gmail.com>
2025-09-22 10:49:07 +0530

    add straigh img

--- digivision/main.py
+++ digivision/main.py
@@ -2,9 +2,14 @@ import test
 import cor
 import ocr_image
 import consume
+import straight
+import matplotlib.pyplot as plt
+
 json_path="Raw_response/response0.json"
-input_path=r"E:\Xbiz_assignment\OCR_segmentaiton\static\docs\Capture.PNG"
-consume.api_consume(input_path)
+input_path=r"E:\Xbiz_assignment\OCR_segmentaiton\static\docs\dhapubal.png"
+angle=straight.compute_text_angle_for_best_word(input_path,draw_result=False)
+path=straight.rotate_image_auto(input_path,angle)
+consume.api_consume(path)
 test.extract_data(json_path)
-ocr_image.makeimg(json_path,input_path)
+ocr_image.makeimg(json_path,path)
 cor.main(json_path)
